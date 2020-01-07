46114c7748e7031ed6190b3a9a018a1e738f2073
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2020-01-07 15:54:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,9 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  ["apples", "oranges", "pears", "lemons"]
-  .push("kiwi")
+  #["apples", "oranges", "pears", "lemons"]
+  #.push("kiwi")
+  array << element 
 end
 
 def add_element_to_start_of_array(array, element)
